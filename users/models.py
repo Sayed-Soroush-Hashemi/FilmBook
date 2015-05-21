f3bb18a736303f1ea2089b2b2eb99ab6fd0809fa
S.HASHEMI <hashemi.soroush@gmail.com>
2015-05-21 21:24:54 +0430

    FBUser models fixed.

--- users/models.py
+++ users/models.py
@@ -4,3 +4,7 @@ from django.contrib.auth.models import User
 
 class FBUser(User):
     nickname = models.CharField(blank=True, null=True, max_length=100)
+    birthday = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
